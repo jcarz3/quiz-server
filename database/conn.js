56c39998584d2ec3764a88a41aa8acfb3d4b43bc
Jason Carzano <poltskie119@gmail.com>
2023-11-04 15:37:08 +0800

    final

--- database/conn.js
+++ database/conn.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export default async function connect() {
+  mongoose.set("strictQuery", false);
   await mongoose.connect(
     "mongodb+srv://poltskie119:QPUIH6OkOsniAssa@cluster0.5mwzizq.mongodb.net/?retryWrites=true&w=majority"
   );

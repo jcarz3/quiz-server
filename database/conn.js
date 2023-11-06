b060b6d6a9f32396568132d83d47cd9c064979fd
Jason Carzano <poltskie119@gmail.com>
2023-11-06 10:14:34 +0800

    final fnata

--- database/conn.js
+++ database/conn.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-export default async function connect() {
+export default function connect() {
   mongoose.set("strictQuery", false);
-  await mongoose.connect(
+  mongoose.connect(
     "mongodb+srv://poltskie119:QPUIH6OkOsniAssa@cluster0.5mwzizq.mongodb.net/?retryWrites=true&w=majority"
   );
   console.log("Database Connected");
--- server.js
+++ server.js
@@ -29,17 +29,18 @@ app.get("/", (req, res) => {
   }
 });
 
+app.listen(port, console.log(`Listening on port ${port}...`));
 /** start server only when we have valid connection */
-connect()
-  .then(() => {
-    try {
-      app.listen(port, () => {
-        console.log(`Server connected to http://localhost:${port}`);
-      });
-    } catch (error) {
-      console.log("Cannot connect to the server");
-    }
-  })
-  .catch((error) => {
-    console.log("Invalid Database Connection");
-  });
+// connect()
+//   .then(() => {
+//     try {
+//       app.listen(port, () => {
+//         console.log(`Server connected to http://localhost:${port}`);
+//       });
+//     } catch (error) {
+//       console.log("Cannot connect to the server");
+//     }
+//   })
+//   .catch((error) => {
+//     console.log("Invalid Database Connection");
+//   });
